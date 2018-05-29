04860da15c70ea121e0f7fc14eb69b208e020001
Jes Ababon <jesababon@gmail.com>
2018-05-29 15:05:06 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 const express = require("express");
-
+const session = require("express-session");
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 4567;
-
 const app = express();
+
+app.use(
+  session({
+    secret: "some random string we should change for our application",
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 
 app.set("view engine", "ejs");
 
-app.get("/", (request, response) => {
-  response.render("home/index", { backgroundColor: "#F4FFFC" });
+app.get("/", (request, response) => {  
+  const favoriteColor = request.session.favoriteColor;
+  response.render("home/index", { 
+    backgroundColor: favoriteColor,
+    fontFamily: request.session.fontFamily
+  
+  });
 });
 
-app.post("/set-favorite-color", (request, response) => {});
+app.post("/set-favorite-color", (request, response) => {
+  console.log(request.body);
+  
+  const favoriteColor = request.session.favoriteColor;
+  request.session.favoriteColor = request.body["favorite-color"];
+  request.session.fontFamily = request.body["font-family-dropdown"];
+  response.redirect(302, '/');
+  });
 
 app.listen(PORT, () => {
   console.log(`Express web server listening on port ${PORT}`);
